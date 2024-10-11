162fca287ed4fa5732784776f152cd88714504d5
Andineers <andinirizky91@gmail.com>
2024-10-12 00:29:49 +0700

    logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: '15px'
+      }
+    },
+    screen: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+      xl: '1330px',
+    },
+    extend: {
+      colors: {
+        primary: '#242a2b',
+        secondary: '#808080',
+        accent: {
+          DEFAULT:'#1cbccf',
+          secondary: '#18abbc',
+          tertiary: '#90c6cd',
+        },
+        grey:'#6F727A',
+    },
+    fontFamily: {
+      primary: 'Poppins'
+  },
+  boxShadow: {
+    custom1: '0px 2px 40px 0px rgba(8, 70, 78, 0.08)',
+    custom2: '0px 0px 30px 0px rgba(8, 73, 81, 0.06)',
+  }
+},
   },
   plugins: [],
-}
+  };
